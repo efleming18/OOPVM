9431e9381b7327de59303082a866b6b5902dfeb7
adomfritz <gaf13@zips.uakron.edu>
2015-05-06 13:14:25 -0400

    updste

--- instruction.hpp
+++ instruction.hpp
@@ -6,59 +6,139 @@
 
 #include <vector>
 
-class add
+
+struct Arithmetic_inst 
 {
-public:
-	 add(int, int, int);
-	 addi(int, int, int);
-	
+	int write_reg;
+	int reg_a;
+	int reg_b;
 };
 
-class sub
+struct Immd_arithmetic_inst 
 {
-public:
-	 sub(int, int, int);
-	 subi(int, int, int);
-	
+	int write_reg;
+	int reg_a;
+	int immd;
+};
+
+struct Jump
+{
+	int target;
+};
+
+struct Branch
+{
+	int condition_reg;
+	int target;
+};
+
+struct Access
+{
+	int reg;
+	int memory;
+};
+
+struct Instruction
+{
+ enum { add, addi, sub, subi, mult, multi, div, divi, load, store, bnez, beqz, jump } type;
+ union 
+ 	{
+	 	Immd_arithmetic_inst iai;
+	 	Branch br;
+	 	Jump j;
+	 	Arithmetic_inst ai;
+	 	Access ac;
+	 } op;
 };
 
 
-class mult
+/*
+struct instruction
 {
-public:
-	 mult(int, int, int);
-	 multi(int, int, int);
-	
-};
+	void Add(int write_reg, int reg_a, int reg_b)
+	{
+		int result;
+		result = value(reg_a) + value(reg_b);
+		store(write_reg, result);
+	}
 
-class div
-{
-public:
-	 div(int, int, int);
-	 divi(int, int, int);
-	
-};
+	void Addi(int write_reg, int reg_a, int immd)
+	{
+		int result;
+		result = value(reg_a) + immd;
+		store(write_reg, result);
+	}
 
-class mem_access
-{
-public:
-	load(int, int);
-	store(int, int);
-};
+	void Sub(int write_reg, int reg_a, int reg_b)
+	{
+		int result;
+		result = value(reg_a) - value(reg_b);
+		store(write_reg, result);
+	}
 
-class branch
-{
-public:
-	bnez(std::vector, int, int);
-	beqz(std::vector, int, int);
+	void Subi (int write_reg, int reg_a, int immd)
+	{
+		int result;
+		result = value(reg_a) - immd;
+		store(write_reg, result);
+	}
+
+	void Mult(int write_reg, int reg_a, int reg_b)
+	{
+		int result;
+		result = value(reg_a) * value(reg_b);
+		store(write_reg, result);
+	}
+
+	void Multi (int write_reg, int reg_a, int immd)
+	{
+		int result;
+		result = value(reg_a) * immd;
+		store(write_reg, result);
+	}
+
+	void Div(int write_reg, int reg_a, int reg_b)
+	{
+		int result;
+		result = value(reg_a) / value(reg_b);
+		store(write_reg, result);
+	}
+
+	void Divi (int write_reg, int reg_a, int immd)
+	{
+		int result;
+		result = value(reg_a) / immd;
+		store(write_reg, result);
+	}
+
+	void Read_mem (int reg, int memory)
+	{
+		reg = readWord(memory);
+	}
+
+	void Write_mem (int reg, int memory)
+	{
+		writeWord(memory, reg);
+	}
+
+	void Bnez(std::vector &myvector, int target, int reg)
+	{
+		if (value(reg) == 0)
+			jump.jump(myvector, target);
+	}
+
+	void Beqz(std::vector &myvector, int target, int reg)
+	{
+		if (value(reg) != 0)
+			jump.jump(myvector, target);
+	}
+
+	void Jump(std::vector &myvector, int target)
+	{
+		//This isnt c++ need to write someting to access vector at target
+		//myvector.at(target);
+	}
 }
-
-class jump
-{
-public:
-	jump(std::vector, int);
-}
+*/
 
 #endif
-
-int number_switch(int);
